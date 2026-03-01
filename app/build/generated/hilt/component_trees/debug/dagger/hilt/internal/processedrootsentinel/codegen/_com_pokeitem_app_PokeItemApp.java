6ce10ceed2f770eb2ee29c0f4435db5a6ec324b7
mirevuns <mirevuns3762@gmail.com>
2026-03-02 03:06:26 +1000

    chore: remove generated files from repo

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_pokeitem_app_PokeItemApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.pokeitem.app.PokeItemApp"
-)
-public final class _com_pokeitem_app_PokeItemApp {
-}
